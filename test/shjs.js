0d6e32a9692b0293ad2e4e5d24cb3a00210a8718
Nate Fischer <ntfschr@google.com>
2025-05-05 17:10:31 -0700

    Update dependencies and package
    This updates the package to modern standards.

--- test/shjs.js
+++ test/shjs.js
@@ -1,7 +1,8 @@
-import path from 'path';
+const path = require('path');
 
-import shell from 'shelljs';
-import test from 'ava';
+const test = require('ava');
+
+const shell = require('shelljs');
 
 const binPath = path.resolve(__dirname, '../bin/shjs');
 
@@ -58,5 +59,7 @@ test('Extension detection', t => {
 //
 
 test('disallow require-ing', t => {
-  t.throws(() => require(binPath), 'Executable-only module should not be required');
+  t.throws(() => require(binPath),
+    { instanceOf: Error },
+    'Executable-only module should not be required');
 });
